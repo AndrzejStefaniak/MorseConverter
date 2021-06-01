79c3e5fa854e0576b30985e8707f16d2827257cb
Andrzej Stefaniak <andrzejstefaniak@outlook.com>
2021-06-01 12:17:12 +0200

    Initial commit

---
+++ src/MorseConverter/Main.java
@@ -0,0 +1,10 @@
+package MorseConverter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.menu();
+    }
+}
---
+++ src/MorseConverter/Menu.java
@@ -0,0 +1,8 @@
+package MorseConverter;
+
+public class Menu {
+
+    public static void menu() {
+        System.out.println("test");
+    }
+}
